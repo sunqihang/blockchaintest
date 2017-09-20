c53f95c4dceb4efeb2e57911f6c49b0376cbadcf
sunqihang <qihangsun2012@gmail.com>
2017-09-20 11:26:00 -0400

    Add files via upload

---
+++ Concat.sol
@@ -0,0 +1,17 @@
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
+    
+    library Concat {
+    using strings for *;
+    //string s3 = ",";
+
+    function concatList(string s1, string s2) internal returns (string) {
+        bytes memory string1 = bytes(s1);
+        string memory s4;
+        if (string1.length == 0) {
+            s4 = "";
+        } else {
+            s4 = s1.toSlice().concat(",".toSlice());
+        }
+        return s4.toSlice().concat(s2.toSlice());
+    }
+    }
